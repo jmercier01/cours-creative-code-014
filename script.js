0619babf1f4dbac0776dd276f17bb1a673f9cb3e
Jerome Mercier <coding@jmercier.fr>
2023-12-03 22:05:01 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const lenis = new Lenis()
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+gsap.from("#two", {
+  scrollTrigger: {
+    trigger: "#two",
+    start: "0% 66%",
+    end: "50% 33%",
+    scrub : false,
+    markers: true,
+    toggleActions: "play reverse play reverse"
+  },
+  scale:0.5,
+  opacity: 0,
+  duration:.5 // marche que si scrub est a false
+});
